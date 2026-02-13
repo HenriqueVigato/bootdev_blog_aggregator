3624977b1663f6401f9b876f05c2292ce5e51a43
Henrique <henriquevigato@gmail.com>
2026-02-13 09:00:49 -0300

    Implementando o main.go e colocando o binario no gitignore

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/HenriqueVigato/bootdev_blog_aggregator/internal/config"
+)
+
 func main() {
+	document, _ := config.Read()
+	fmt.Printf("%+v\n", document)
+	_ = config.SetUser("HenriqueVigato")
+
+	document, _ = config.Read()
+	fmt.Printf("%+v\n", document)
 }
