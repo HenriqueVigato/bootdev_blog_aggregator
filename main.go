4a6f90c6c76005121a34a2a7bf8c2e7393133909
henrique vigato <henriquevigato@gmail.com>
2026-02-17 15:46:42 -0300

    Atualizando o main para completar a funcao

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/HenriqueVigato/bootdev_blog_aggregator/internal/config"
 )
@@ -11,13 +12,31 @@ type state struct {
 }
 
 func main() {
-	document, err := config.Read()
+	cfg, err := config.Read()
 	if err != nil {
-		fmt.Errorf("err: %v", err)
+		log.Fatal("err: ", err)
+	}
+
+	programState := &state{
+		cfg: cfg,
 	}
 
 	cmds := &commands{
 		registercommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: gator <command> [args...]")
+	}
+
+	cmd := command{
+		Name: os.Args[1],
+		Args: os.Args[2:],
+	}
+
+	err = cmds.run(programState, cmd)
+	if err != nil {
+		log.Fatal("erro com a chamda da funcionalidado: ", err)
+	}
 }
