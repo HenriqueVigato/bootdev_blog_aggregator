9e42bbec455dec17b0c5cb0925992862b742f985
henrique vigato <henriquevigato@gmail.com>
2026-02-17 21:00:55 -0300

    Criando o handler para registros de novos usuarios

--- handler_user.go
+++ handler_user.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/HenriqueVigato/bootdev_blog_aggregator/internal/config"
+	"github.com/HenriqueVigato/bootdev_blog_aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -15,5 +20,34 @@ func handlerLogin(s *state, cmd command) error {
 		return err
 	}
 	fmt.Println("User has been set.")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) == 0 {
+		return fmt.Errorf("o register handler esepara um argumento")
+	}
+	ctx := context.Background()
+
+	_, err := s.db.GetUser(ctx, cmd.Args[0])
+	if err == nil {
+		return fmt.Errorf("o usuario '%s' ja existe", cmd.Args[0])
+	}
+
+	if err != sql.ErrNoRows {
+		return err
+	}
+
+	newUser := &database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.Args[0],
+	}
+	user, err := s.db.CreateUser(ctx, *newUser)
+	if err != nil {
+		return err
+	}
+	fmt.Println("The user was creatted: ", user)
 	return nil
 }
