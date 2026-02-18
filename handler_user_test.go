0afb76af0aa3641ad0a3bb9cb42456982a1b3f72
henrique vigato <henriquevigato@gmail.com>
2026-02-17 21:00:29 -0300

    Crianto testes para o handler register e um bando de dados no docker para os testes

--- handler_user_test.go
+++ handler_user_test.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"database/sql"
 	"io"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/HenriqueVigato/bootdev_blog_aggregator/internal/config"
+	"github.com/HenriqueVigato/bootdev_blog_aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func setupTestState() (*state, command) {
@@ -40,6 +45,22 @@ func setupTestEnv(t *testing.T) string {
 	})
 
 	return tmpDir
+}
+
+func setupTestDB(t *testing.T) (*database.Queries, func()) {
+	dbURL := "postgres://gator_user:gator_pass@localhost:5433/gator?sslmode=disable"
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		t.Fatalf("erro ao conectar no banco de testes: %v", err)
+	}
+
+	dbQueries := database.New(db)
+	cleanup := func() {
+		db.Exec("DELETE FROM users")
+		db.Close()
+	}
+	return dbQueries, cleanup
 }
 
 func capturaOutput(fn func() error) (string, error) {
@@ -94,5 +115,64 @@ func TestHandlerLogin_ValidUserName(t *testing.T) {
 	if !strings.Contains(output, "been set") {
 		t.Logf("output: %v", output)
 		t.Fatalf("Esperava a mensagem de sucesso")
+	}
+}
+
+func TestHandlerRegister_FaltaArgumento(t *testing.T) {
+	dbQueries, cleanup := setupTestDB(t)
+	defer cleanup()
+
+	s := &state{db: dbQueries}
+	cmd := command{Name: "register", Args: []string{}}
+
+	err := handlerRegister(s, cmd)
+	if err == nil {
+		t.Fatalf("esperava erro quando nao ha argumentos")
+	}
+}
+
+func TestHandlerRegister_UsuarioJaExiste(t *testing.T) {
+	dbQueries, cleanup := setupTestDB(t)
+	defer cleanup()
+
+	s := &state{db: dbQueries}
+	ctx := context.Background()
+	_, err := dbQueries.CreateUser(ctx, database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      "Test User",
+	})
+	if err != nil {
+		t.Fatalf("erro ao criar o usuario de test %v", err)
+	}
+
+	cmd := command{Name: "register", Args: []string{"Test User"}}
+	err = handlerRegister(s, cmd)
+
+	if err == nil {
+		t.Fatalf("esperava um erro pois o usuario ja existe")
+	}
+
+	if !strings.Contains(err.Error(), cmd.Args[0]) {
+		t.Errorf("Esperava que tivesse uma mensagem de sucesso com o nome do usuario criado, mas recebeu %v", err.Error())
+	}
+}
+
+func TestHandlerRegister_Success(t *testing.T) {
+	dbQueries, cleanup := setupTestDB(t)
+	defer cleanup()
+
+	s := &state{db: dbQueries}
+	cmd := command{Name: "register", Args: []string{"test_user_success"}}
+
+	err := handlerRegister(s, cmd)
+	if err != nil {
+		t.Fatalf("nao era esperado nenhum erro")
+	}
+
+	_, err = s.db.GetUser(context.Background(), "test_user_success")
+	if err != nil {
+		t.Fatalf("nao era esperado nao encontar nenhum erro")
 	}
 }
