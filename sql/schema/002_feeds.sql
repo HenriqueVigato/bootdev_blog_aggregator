208ae85907dc6f12d034cdd1e312cf98ce688ac4
Henrique <henriquevigato@gmail.com>
2026-02-25 11:43:11 -0300

    Criando a tabela dos feeds

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE feeds (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  name VARCHAR(255) NOT NULL,
+  url VARCHAR(255) NOT NULL UNIQUE,
+  user_id UUID NOT NULL,
+
+  FOREIGN KEY(user_id) REFERENCES users(id)
+    ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE feeds;
