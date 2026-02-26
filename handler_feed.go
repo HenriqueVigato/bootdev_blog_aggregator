078107d9be62134d2b2c1b9727da2af7049dec91
Henrique <henriquevigato@gmail.com>
2026-02-26 11:47:45 -0300

    Criando mais testes para a funcao addFeed

--- handler_feed.go
+++ handler_feed.go
@@ -1,10 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/HenriqueVigato/bootdev_blog_aggregator/internal/database"
+	"github.com/google/uuid"
+)
 
 func addFeed(s *state, cmd command) error {
+	ctx := context.Background()
 	if len(cmd.Args) < 2 {
 		return fmt.Errorf("o handler addFeed espera o nome do feed e a URL")
+	}
+	getCurrentUser, err := s.db.GetUser(ctx, s.cfg.CurrentUserName)
+	if err != nil {
+		return err
+	}
+
+	newFeed := &database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.Args[0],
+		Url:       cmd.Args[1],
+		UserID:    getCurrentUser.ID,
+	}
+
+	_, err = s.db.CreateFeed(ctx, *newFeed)
+	if err != nil {
+		return err
 	}
 	return nil
 }
