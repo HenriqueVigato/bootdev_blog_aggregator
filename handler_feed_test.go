1af7458fa66e1b163c1b39a9e3b0e8663be8ed18
Henrique <henriquevigato@gmail.com>
2026-02-26 11:47:53 -0300

    Implementando a funcao add feed

--- handler_feed_test.go
+++ handler_feed_test.go
@@ -11,3 +11,37 @@ func TestAddFeed_emptyUser(t *testing.T) {
 		t.Error("era esperado uma mensagem de erro falando que esta faltando argumentos")
 	}
 }
+
+func TestAddFeed_success(t *testing.T) {
+	s, cmd, _ := setupTestState(t)
+	cmd.Args = []string{"Hacker News RSS", "https://hnrss.org/newst"}
+
+	_, err := capturaOutput(func() error {
+		return addFeed(s, cmd)
+	})
+	if err != nil {
+		t.Logf("Err: %v", err)
+		t.Errorf("Nao era esperado uma mensagem de erro")
+	}
+}
+
+func TestAddFeed_duplicateUser(t *testing.T) {
+	s, cmd, _ := setupTestState(t)
+	cmd.Args = []string{"Hacker News RSS", "https://hnrss.org/newst"}
+
+	_, err := capturaOutput(func() error {
+		return addFeed(s, cmd)
+	})
+	if err != nil {
+		t.Logf("Err: %v", err)
+		t.Errorf("nao era esperado um erro nesta etapa")
+	}
+
+	_, err = capturaOutput(func() error {
+		return addFeed(s, cmd)
+	})
+	if err == nil {
+		t.Logf("Err: %v", err)
+		t.Errorf("era esperado um erro pois o feed esta duplicado")
+	}
+}
