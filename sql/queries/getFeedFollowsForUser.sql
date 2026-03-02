74e88fb3c3c50650b386c0956f120366373aaefa
henrique vigato <henriquevigato@gmail.com>
2026-03-01 21:10:45 -0300

    criando uma querie que buscar os feeds que estao followed

---
+++ sql/queries/getFeedFollowsForUser.sql
@@ -0,0 +1,7 @@
+-- name: GetFeedFollowsForUser :many
+SELECT feed_follows.*, feeds.name AS feed_name, users.name AS user_name
+  FROM feed_follows
+INNER JOIN feeds ON feeds.id = feed_follows.feed_id
+INNER JOIN users on users.id = feed_follows.user_id
+WHERE users.id = $1
+ORDER BY feeds.name ASC;
