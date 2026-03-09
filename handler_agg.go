004abc3eee52ed211887f4583e746dd7e64fb606
henrique vigato <henriquevigato@gmail.com>
2026-03-08 22:13:05 -0300

    implementando o scrapeFeeds e atualizando o agg e criando os testes

--- handler_agg.go
+++ handler_agg.go
@@ -3,16 +3,48 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 func agg(s *state, cmd command) error {
-	_ = s
-	_ = cmd
-	feedURL := "https://www.wagslane.dev/index.xml"
-	feed, err := fetchFeed(context.Background(), feedURL)
-	if err != nil {
-		return err
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("se espera um intervalo de busca dos feeds")
 	}
-	fmt.Printf("%+v\n", feed)
+	interval, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("tempo invalide: %v", err)
+	}
+
+	fmt.Printf("Collecting feeds every %v\n", interval)
+
+	ticker := time.NewTicker(interval)
+
+	for ; ; <-ticker.C {
+		if err = scrapeFeeds(s); err != nil {
+			return err
+		}
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	ctx := context.Background()
+	feedsToFetch, err := s.db.GetNextFeedToFetch(ctx)
+	if err != nil {
+		return fmt.Errorf("GetNextFeedToFetch error: %v", err)
+	}
+
+	if err = s.db.MarkFeedFetched(ctx, feedsToFetch.ID); err != nil {
+		return fmt.Errorf("marking feed fetched error: %v", err)
+	}
+
+	feed, err := fetchFeed(ctx, feedsToFetch.Url)
+	if err != nil {
+		return fmt.Errorf("error in getting the feed: %v", err)
+	}
+
+	for i, item := range feed.Channel.Item {
+		fmt.Printf("%d- %s\n", i, item.Title)
+	}
+
 	return nil
 }
--- handler_agg_test.go
+++ handler_agg_test.go
@@ -1,21 +1,60 @@
 package main
 
 import (
+	"context"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestAgg(t *testing.T) {
 	s, cmd, _ := setupTestState(t)
+	cmd.Args = []string{"5s"}
+
 	output, err := capturaOutput(func() error {
-		return agg(s, cmd)
+		go agg(s, cmd)
+		time.Sleep(2 * time.Second)
+		return nil
 	})
 	if err != nil {
-		t.Fatalf("erro ao executar o comando agg, %v", err)
+		t.Fatalf("erro com a funcao agg: %v", err)
 	}
 
-	if !strings.Contains(output, "Lane's Blog") {
-		t.Logf("output \n %v", output)
-		t.Errorf("output diferente do esperado")
+	if !strings.Contains(output, "5-") {
+		t.Fatalf("era esperado o titulo Nobody Care mas recebeu: \n%v", output)
+	}
+}
+
+func TestScrapeFeeds(t *testing.T) {
+	s, _, _ := setupTestState(t)
+	ctx := context.Background()
+
+	feeds, err := s.db.GetNextFeedToFetch(ctx)
+	if err != nil {
+		t.Logf("erro ao buscar os feeds")
+	}
+
+	if feeds.LastFetchedAt.Valid {
+		t.Fatalf("Nao era esperado nenhum valor alem de null")
+	}
+
+	output, err := capturaOutput(func() error {
+		return scrapeFeeds(s)
+	})
+	if err != nil {
+		t.Fatalf("erro ao scrapeFeeds: %v", err)
+	}
+
+	if !strings.Contains(output, "10-") {
+		t.Fatalf("esperava o titulo dos feeds itens mas recebeu: \n%v", output)
+	}
+
+	feeds, err = s.db.GetFeedByURL(ctx, feeds.Url)
+	if err != nil {
+		t.Logf("erro ao buscar os feeds")
+	}
+
+	if !feeds.LastFetchedAt.Valid {
+		t.Fatalf("era esperado a data que o feed foi buscado: %v", feeds.LastFetchedAt)
 	}
 }
